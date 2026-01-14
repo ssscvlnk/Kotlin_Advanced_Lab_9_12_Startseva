4ad1a11d4c656739ab8e8c1d33f410b63fb144d9
ssscvlnk <pstartceva@yandex.ru>
2026-01-14 16:31:26 +0300

    Add abstract modules, interfaces ang first module implementations

---
+++ src/OutpostModule.kt
@@ -0,0 +1,10 @@
+abstract class OutpostModule(
+    val name: String,
+    var level: Int = 1
+) {
+    fun upgrade() {
+        level++
+        println("$name удучшен до уровня $level")
+    }
+    abstract fun performAction(manager: ResourceManager)
+}
---
+++ src/ResearchLab.kt
@@ -0,0 +1,11 @@
+class ResearchLab : OutpostModule( "Исследовательская лаборатория") {
+    override fun performAction(manager: ResourceManager) {
+        val minerals = manager.get("Minerals")
+        if (minerals == null || minerals.amount<30) {
+            println("Недостаточно минералов для исследования")
+            return
+        }
+        minerals.amount-=30
+        println("Лаборатория проводит исследование (минералы -30)")
+    }
+}
