e35cfcb6508bfe3e3ff3b2d25d7c30d2e42a4bb8
ssscvlnk <pstartceva@yandex.ru>
2026-01-28 13:22:32 +0300

    add delegation, lazy, observer and state saving practice

---
+++ src/outpostState/OutpostManager.kt
@@ -0,0 +1,11 @@
+package outpostState
+
+class OutpostManager {
+    val resources = mutableListOf<ObservableResource>()
+    init {
+        println("Менеджер создан")
+    }
+}
+val manager: OutpostManager by lazy {
+    OutpostManager()
+}
